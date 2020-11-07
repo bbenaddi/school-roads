d18c1ecb0e774794f15bbf8eda9ab2148b950651
Brahim Ben Addi <brahim.benaddi98@gmail.com>
2020-11-07 21:37:13 +0100

    init Express

---
+++ server/index.js
@@ -0,0 +1,16 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+var port = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+var indexRouter = require('./routes/index');
+
+app.use('/', indexRouter);
+
+app.listen(port, () => {
+    console.log(`School Roads App listening at http://localhost:${port}`);
+})
