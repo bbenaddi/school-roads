aa4f54ec1a6bbf842ae04f34a2f7c79480034498
Brahim Ben Addi <brahim.benaddi98@gmail.com>
2020-11-07 22:00:43 +0100

    adding postgres conenction

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,37 @@
-var express = require('express');
+var express = require('express'); 
 var router = express.Router();
 
-router.get('/', function (req, res) {
-    res.send('Index Page');
+const { Client, Query } = require('pg')
+
+// Setup connection
+var username = "postgres"
+var password = "root" 
+var host = "localhost:5432"
+var database = "nyc" 
+var conString = "postgres://"+username+":"+password+"@"+host+"/"+database; // Your Database Connection
+
+var client = new Client(conString);
+client
+  .connect()
+  .then(() => console.log('Database connected'))
+  .catch(err => console.error('connection error', err.stack))
+
+var request = "SELECT * FROM nyc_census_blocks";
+
+router.get('/data', function (req, res) {
+  var query = client.query(new Query(request));
+  query.on("row", function (row, result) {
+      result.addRow(row);
+  });
+
+  query.on("end", function (result) {
+      res.send(result.rows);
+      res.end();
+  });
+});
+
+router.get('/', function(req, res, next) {
+  res.send('Index Page');
 });
 
 module.exports = router;
